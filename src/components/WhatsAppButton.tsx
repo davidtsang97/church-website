1c6e6657093c74f4d12f577f6682fd221ce12e8f
davidtsang97 <davidtsanglylfp@gmail.com>
2025-04-12 00:38:46 +0800

    Update WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -8,7 +8,7 @@ export default function WhatsAppButton() {
   return (
     <div className="fixed bottom-6 right-6 z-50">
       <a
-        href="https://wa.me/447445056274?text=你好%20我想了解一下伯明翰主日聚會資訊"
+        href="https://wa.me/85293412164?text=你好%20我想了解一下伯明翰主日聚會資訊"
         target="_blank"
         rel="noopener noreferrer"
         className={`flex items-center justify-center w-14 h-14 rounded-full bg-green-500 shadow-lg transition-all duration-300 transform hover:scale-110 ${
