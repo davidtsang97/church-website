660c4e9f6c137c225981ff79d25c777316848b85
davidtsang97 <davidtsanglylfp@gmail.com>
2025-04-12 02:41:43 +0800

    save2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true
   },
+  trailingSlash: true,
+  distDir: 'out',
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/join/birmingham': { page: '/join/birmingham' }
+    }
+  }
 }
 
 module.exports = nextConfig
